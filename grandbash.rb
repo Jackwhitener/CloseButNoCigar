8f9920ee1fe64e455815b328de7f492ac638c0cb
Jackwhitener <jackwhitener1@gmail.com>
2018-10-11 13:11:55 -0400

    changes

--- grandbash.rb
+++ grandbash.rb
@@ -1,20 +1,39 @@
-def multiple_tickets(my_numbers, bash_numbers)
-	count = 0
-	x = 0
-	y = 0
-	in_my_numbers = my_numbers[x]
-	in_bash_numbers = bash_numbers[x]
-	my_numbers.each do
-		in_my_numbers.length.times do
-			if in_my_numbers[y] == in_bash_numbers[y]
-				count += 1
-			end
+# def multiple_tickets(my_numbers, bash_numbers)
+# 	count = 0
+# 	x = 0
+# 	y = 0
+# 	in_my_numbers = my_numbers[x]
+# 	in_bash_numbers = bash_numbers[x]
+# 	my_numbers.each do
+# 		in_my_numbers.length.times do
+# 			if in_my_numbers[y] == in_bash_numbers[y]
+# 				count += 1
+# 			end
+# 		end
+# 		if count == in_my_numbers.length
+# 			return in_my_numbers
+# 		end
+
+# 	end
+# 	puts count
+# end
+# multiple_tickets(["1234"],["1234"])
+def grand_bash(my_number, bash_numbers)
+	puts "my number: #{my_number}"
+	puts "bash number: #{bash_numbers}"
+	arr1 = []
+	my_number.each do |item|
+		puts item
+		if bash_numbers.include?(item)
+			arr1<< item
 		end
-		if count == in_my_numbers.length
-			return in_my_numbers
-		end
-		
+		return arr1
 	end
-	puts count
+
+	# arr1 = []
+	# if bash_numbers.include?(my_number)
+	# 	arr1<< my_number
+	# end
+	# arr1
+
 end
-multiple_tickets(["1234"],["1234"])
--- grandbashtest.rb
+++ grandbashtest.rb
@@ -2,8 +2,8 @@ require "minitest/autorun"
 require_relative "grandbash.rb"
 class My_test < Minitest::Test
 	def test_one
-		my_numbers = ["1234"]
-		bash_numbers = ["1234"]
-		assert_equal("1234", multiple_tickets(my_numbers, bash_numbers))
+		my_number = ["1234", "4567", "9999"]
+		bash_numbers = ["1234", "1345", "5623", "8888"]
+		assert_equal(["1234"], grand_bash(my_number, bash_numbers))
 	end
 end
